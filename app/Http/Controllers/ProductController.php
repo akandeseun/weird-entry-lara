78836e4a3d7f1cde7841c642a858478ee59be295
akandeseun <akandeseun44@gmail.com>
2023-11-27 13:29:43 +0100

    remove unecessary stuff

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use App\Services\ProductService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 
 class ProductController extends Controller
 {
