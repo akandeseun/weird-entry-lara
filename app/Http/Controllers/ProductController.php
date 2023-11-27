30ce45939a0952e234661ee7633624f526ed4eb8
akandeseun <akandeseun44@gmail.com>
2023-11-27 10:07:54 +0100

    refactor validator to use facades

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -30,24 +31,27 @@ class ProductController extends Controller
 
     public function createProduct(Request $request)
     {
-        $validatedData = $request->validate([
-            'title' => 'required|string',
-            'description' => 'required|string',
-            'product_image' => 'required|string',
-            'size_id' => 'required|array|exists:sizes,id',
-            'color_id' => 'required|array|exists:colors,id',
-            'price' => 'required|integer',
-            'sales_price' => 'sometimes|integer',
-            'featured' => 'sometimes|boolean',
-            'category_id' => 'required|integer|exists:categories,id'
-        ]);
+
+        Validator::make($request->all(), [
+            'title' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'product_image' => ['required', 'string'],
+            'size_id' => ['required', 'array', 'exists:sizes,id'],
+            'color_id' => ['required', 'array', 'exists:colors,id'],
+            'price' => ['required', 'integer'],
+            'sales_price' => ['sometimes', 'integer'],
+            'featured' => ['sometimes', 'boolean'],
+            'category_id' => ['required', 'integer', 'exists:categories,id']
+        ])->validate();
+
+        // $validatedData = $request->validate([]);
 
         $product = Product::create($request->all());
 
         // $product->with('category');
 
-        $product->sizes()->attach($validatedData['size_id']);
-        $product->colors()->attach($validatedData['color_id']);
+        $product->sizes()->attach($request->size_id);
+        $product->colors()->attach($request->color_id);
 
         return response([
             "data" => $product,
@@ -57,20 +61,21 @@ class ProductController extends Controller
 
     public function updateProduct(Request $request, $id)
     {
-        $validatedData = $request->validate([
-            'title' => 'string',
-            'description' => 'string',
-            'product_image' => 'string',
-            'size_id' => 'integer|exists:sizes,id',
-            'colors' => 'string',
-            'price' => 'integer',
-            'featured' => 'boolean',
-            'category_id' => 'integer|exists:categories,id'
-        ]);
+        Validator::make($request->all(), [
+            'title' => ['string'],
+            'description' => ['string'],
+            'product_image' => ['string'],
+            'size_id' => ['array', 'exists:sizes,id'],
+            'color_id' => ['array', 'exists:colors,id'],
+            'price' => ['integer'],
+            'sales_price' => ['sometimes', 'integer'],
+            'featured' => ['sometimes', 'boolean'],
+            'category_id' => ['integer', 'exists:categories,id']
+        ])->validate();
 
         $product = Product::findOrFail($id);
 
-        $product->update($validatedData);
+        $product->update($request->all());
 
         return response([
             "message" => "Product Updated Successfully",
