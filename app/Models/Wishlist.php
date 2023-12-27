b5b30dbd049f22fe5212a090d0a097c1201447f4
akandeseun <akandeseun44@gmail.com>
2023-12-27 19:12:38 +0100

    wishlists relationships

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Wishlist extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function products(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
