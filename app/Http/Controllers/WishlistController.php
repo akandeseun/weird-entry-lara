ed7d930a0e4247efb1ebbc395ffa5abe8fd3dd5e
akandeseun <akandeseun44@gmail.com>
2023-12-27 21:07:44 +0100

    feature: wishlist

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class WishlistController extends Controller
 {
-    //
+    public function addToWishList(Request $request)
+    {
+        Validator::make($request->all(), [
+            'product_id' => ['required', 'integer', 'exists:products,id'],
+        ])->validate();
+
+        $wishlist = Wishlist::create([
+            "user_id" => Auth::id(),
+            "product_id" => $request->product_id
+        ]);
+
+        return response()->json([
+            "message" => "Product added to wishlist",
+            $wishlist
+        ]);
+    }
+
+    public function removeFromWishList(Request $request)
+    {
+        $user = Auth::id();
+        Validator::make($request->all(), [
+            'product_id' => ['required', 'integer', 'exists:products,id'],
+        ])->validate();
+
+        $wishlists = Wishlist::where('user_id', $user)
+            ->where('product_id', $request->product_id)->delete();
+
+        return response()->json([
+            "message" => "item deleted from wishlist"
+        ]);
+    }
+
+    public function getCurrentUserWishlist()
+    {
+        $user = Auth::id();
+
+        $wishlists = Wishlist::where('user_id', $user)->get();
+
+        if ($wishlists->isEmpty()) {
+            return response()->json(["message" => "Empty Wishlist"]);
+        }
+
+        return response()->json($wishlists);
+    }
 }
