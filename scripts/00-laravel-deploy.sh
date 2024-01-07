54716993bc7bbf84f6712bb04e8df3e5f2ca92c6
akandeseun <akandeseun44@gmail.com>
2024-01-07 10:54:51 +0100

    minor changes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -12,5 +12,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
