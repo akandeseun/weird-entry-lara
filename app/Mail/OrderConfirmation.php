41632e643cd775ecc425ff0996522579acb15b13
akandeseun <akandeseun44@gmail.com>
2024-01-22 10:18:05 +0100

    change mailable

--- app/Mail/OrderConfirmation.php
+++ app/Mail/OrderConfirmation.php
@@ -37,7 +37,7 @@ class OrderConfirmation extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.orders.shipped',
         );
     }
 
--- database/migrations/2023_11_29_170406_create_cms_table.php
+++ database/migrations/2023_11_29_170406_create_cms_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('cms', function (Blueprint $table) {
             $table->ulid('id')->primary();
+
+
             $table->timestamps();
         });
     }
