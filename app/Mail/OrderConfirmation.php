42cf645a3ef4b6f176838118aef9071e3e4f43e8
akandeseun <akandeseun44@gmail.com>
2024-01-27 09:33:02 +0100

    q

--- app/Mail/OrderConfirmation.php
+++ app/Mail/OrderConfirmation.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderConfirmation extends Mailable
+class OrderConfirmation extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
