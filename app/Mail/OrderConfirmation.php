ea818f19c9552ee49c694a382c478147cd9acce2
akandeseun <akandeseun44@gmail.com>
2024-01-27 10:35:38 +0100

    remove queue

--- app/Mail/OrderConfirmation.php
+++ app/Mail/OrderConfirmation.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderConfirmation extends Mailable implements ShouldQueue
+class OrderConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
